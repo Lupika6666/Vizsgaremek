1ab473e8e3e4ed57f12c525ab8ad53e67516ee11
Keiichi89 <diobence@gmail.com>
2026-02-15 18:45:14 +0100

    javított sql dump, táblanevek átírva többesszámra

--- konyvtar.sql
+++ konyvtar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2026. Feb 10. 21:00
+-- Létrehozás ideje: 2026. Feb 14. 21:44
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -28,10 +28,10 @@ USE konyvtar;
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `kolcsonzes`
+-- Tábla szerkezet ehhez a táblához `kolcsonzesek`
 --
 
-CREATE TABLE `kolcsonzes` (
+CREATE TABLE `kolcsonzesek` (
   `id` int(11) NOT NULL,
   `kolcsonzes_ideje` date NOT NULL,
   `hatarido` date NOT NULL,
@@ -42,10 +42,10 @@ CREATE TABLE `kolcsonzes` (
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `konyv`
+-- Tábla szerkezet ehhez a táblához `konyvek`
 --
 
-CREATE TABLE `konyv` (
+CREATE TABLE `konyvek` (
   `id` int(11) NOT NULL,
   `cim` varchar(50) NOT NULL,
   `isbn` varchar(13) NOT NULL,
@@ -57,10 +57,10 @@ CREATE TABLE `konyv` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
--- A tábla adatainak kiíratása `konyv`
+-- A tábla adatainak kiíratása `konyvek`
 --
 
-INSERT INTO `konyv` (`id`, `cim`, `isbn`, `leiras`, `publikalas_ev`, `szerzo_id`, `nyelv_id`, `mufaj_id`) VALUES
+INSERT INTO `konyvek` (`id`, `cim`, `isbn`, `leiras`, `publikalas_ev`, `szerzo_id`, `nyelv_id`, `mufaj_id`) VALUES
 (1, 'Földrajz Atlasz középiskolásoknak', '9789633284858', NULL, 2024, NULL, 1, 2),
 (2, 'Digitális kultúra 10.', '9786156256393', NULL, 2021, 1, 1, 1),
 (3, 'Egri Csillagok', '9631139557', NULL, 1985, 2, 1, 4),
@@ -70,19 +70,19 @@ INSERT INTO `konyv` (`id`, `cim`, `isbn`, `leiras`, `publikalas_ev`, `szerzo_id`
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `mufaj`
+-- Tábla szerkezet ehhez a táblához `mufajok`
 --
 
-CREATE TABLE `mufaj` (
+CREATE TABLE `mufajok` (
   `id` int(11) NOT NULL,
   `nev` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
--- A tábla adatainak kiíratása `mufaj`
+-- A tábla adatainak kiíratása `mufajok`
 --
 
-INSERT INTO `mufaj` (`id`, `nev`) VALUES
+INSERT INTO `mufajok` (`id`, `nev`) VALUES
 (2, 'földrajzi atlasz'),
 (3, 'kisregény'),
 (1, 'tankönyv'),
@@ -91,29 +91,29 @@ INSERT INTO `mufaj` (`id`, `nev`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `nyelv`
+-- Tábla szerkezet ehhez a táblához `nyelvek`
 --
 
-CREATE TABLE `nyelv` (
+CREATE TABLE `nyelvek` (
   `id` int(11) NOT NULL,
   `nev` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
--- A tábla adatainak kiíratása `nyelv`
+-- A tábla adatainak kiíratása `nyelvek`
 --
 
-INSERT INTO `nyelv` (`id`, `nev`) VALUES
+INSERT INTO `nyelvek` (`id`, `nev`) VALUES
 (2, 'angol'),
 (1, 'magyar');
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `olvaso`
+-- Tábla szerkezet ehhez a táblához `olvasok`
 --
 
-CREATE TABLE `olvaso` (
+CREATE TABLE `olvasok` (
   `kartyaszam` int(11) NOT NULL,
   `nev` varchar(50) NOT NULL,
   `email` varchar(50) DEFAULT NULL,
@@ -123,10 +123,10 @@ CREATE TABLE `olvaso` (
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `peldany`
+-- Tábla szerkezet ehhez a táblához `peldanyok`
 --
 
-CREATE TABLE `peldany` (
+CREATE TABLE `peldanyok` (
   `id` int(11) NOT NULL,
   `hely` varchar(25) NOT NULL,
   `konyv_id` int(11) NOT NULL
@@ -135,19 +135,19 @@ CREATE TABLE `peldany` (
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `szerzo`
+-- Tábla szerkezet ehhez a táblához `szerzok`
 --
 
-CREATE TABLE `szerzo` (
+CREATE TABLE `szerzok` (
   `id` int(11) NOT NULL,
   `nev` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
--- A tábla adatainak kiíratása `szerzo`
+-- A tábla adatainak kiíratása `szerzok`
 --
 
-INSERT INTO `szerzo` (`id`, `nev`) VALUES
+INSERT INTO `szerzok` (`id`, `nev`) VALUES
 (2, 'Gárdonyi Géza'),
 (3, 'Mikszáth Kálmán'),
 (1, 'Pintér Gergely');
@@ -155,10 +155,10 @@ INSERT INTO `szerzo` (`id`, `nev`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `user`
+-- Tábla szerkezet ehhez a táblához `userek`
 --
 
-CREATE TABLE `user` (
+CREATE TABLE `userek` (
   `nev` varchar(25) NOT NULL,
   `jelszo` varchar(25) NOT NULL,
   `role` varchar(25) NOT NULL,
@@ -170,18 +170,18 @@ CREATE TABLE `user` (
 --
 
 --
--- A tábla indexei `kolcsonzes`
+-- A tábla indexei `kolcsonzesek`
 --
-ALTER TABLE `kolcsonzes`
+ALTER TABLE `kolcsonzesek`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
   ADD KEY `peldany_id` (`peldany_id`),
   ADD KEY `olvaso_id` (`olvaso_id`);
 
 --
--- A tábla indexei `konyv`
+-- A tábla indexei `konyvek`
 --
-ALTER TABLE `konyv`
+ALTER TABLE `konyvek`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
   ADD UNIQUE KEY `isbn` (`isbn`),
@@ -190,48 +190,48 @@ ALTER TABLE `konyv`
   ADD KEY `mufaj_id` (`mufaj_id`);
 
 --
--- A tábla indexei `mufaj`
+-- A tábla indexei `mufajok`
 --
-ALTER TABLE `mufaj`
+ALTER TABLE `mufajok`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
   ADD UNIQUE KEY `nev` (`nev`);
 
 --
--- A tábla indexei `nyelv`
+-- A tábla indexei `nyelvek`
 --
-ALTER TABLE `nyelv`
+ALTER TABLE `nyelvek`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
   ADD UNIQUE KEY `nev` (`nev`);
 
 --
--- A tábla indexei `olvaso`
+-- A tábla indexei `olvasok`
 --
-ALTER TABLE `olvaso`
+ALTER TABLE `olvasok`
   ADD PRIMARY KEY (`kartyaszam`),
   ADD UNIQUE KEY `kartyaszam` (`kartyaszam`);
 
 --
--- A tábla indexei `peldany`
+-- A tábla indexei `peldanyok`
 --
-ALTER TABLE `peldany`
+ALTER TABLE `peldanyok`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
   ADD KEY `konyv_id` (`konyv_id`);
 
 --
--- A tábla indexei `szerzo`
+-- A tábla indexei `szerzok`
 --
-ALTER TABLE `szerzo`
+ALTER TABLE `szerzok`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
   ADD UNIQUE KEY `nev` (`nev`);
 
 --
--- A tábla indexei `user`
+-- A tábla indexei `userek`
 --
-ALTER TABLE `user`
+ALTER TABLE `userek`
   ADD PRIMARY KEY (`nev`),
   ADD UNIQUE KEY `nev` (`nev`),
   ADD KEY `olvaso_id` (`olvaso_id`);
@@ -241,45 +241,45 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT a táblához `kolcsonzes`
+-- AUTO_INCREMENT a táblához `kolcsonzesek`
 --
-ALTER TABLE `kolcsonzes`
+ALTER TABLE `kolcsonzesek`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT a táblához `konyv`
+-- AUTO_INCREMENT a táblához `konyvek`
 --
-ALTER TABLE `konyv`
+ALTER TABLE `konyvek`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT a táblához `mufaj`
+-- AUTO_INCREMENT a táblához `mufajok`
 --
-ALTER TABLE `mufaj`
+ALTER TABLE `mufajok`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT a táblához `nyelv`
+-- AUTO_INCREMENT a táblához `nyelvek`
 --
-ALTER TABLE `nyelv`
+ALTER TABLE `nyelvek`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT a táblához `olvaso`
+-- AUTO_INCREMENT a táblához `olvasok`
 --
-ALTER TABLE `olvaso`
+ALTER TABLE `olvasok`
   MODIFY `kartyaszam` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT a táblához `peldany`
+-- AUTO_INCREMENT a táblához `peldanyok`
 --
-ALTER TABLE `peldany`
+ALTER TABLE `peldanyok`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT a táblához `szerzo`
+-- AUTO_INCREMENT a táblához `szerzok`
 --
-ALTER TABLE `szerzo`
+ALTER TABLE `szerzok`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
@@ -287,31 +287,31 @@ ALTER TABLE `szerzo`
 --
 
 --
--- Megkötések a táblához `kolcsonzes`
+-- Megkötések a táblához `kolcsonzesek`
 --
-ALTER TABLE `kolcsonzes`
-  ADD CONSTRAINT `kolcsonzes_ibfk_1` FOREIGN KEY (`peldany_id`) REFERENCES `peldany` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `kolcsonzes_ibfk_2` FOREIGN KEY (`olvaso_id`) REFERENCES `olvaso` (`kartyaszam`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `kolcsonzesek`
+  ADD CONSTRAINT `kolcsonzesek_ibfk_1` FOREIGN KEY (`peldany_id`) REFERENCES `peldanyok` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `kolcsonzesek_ibfk_2` FOREIGN KEY (`olvaso_id`) REFERENCES `olvasok` (`kartyaszam`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Megkötések a táblához `konyv`
+-- Megkötések a táblához `konyvek`
 --
-ALTER TABLE `konyv`
-  ADD CONSTRAINT `konyv_ibfk_1` FOREIGN KEY (`szerzo_id`) REFERENCES `szerzo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `konyv_ibfk_2` FOREIGN KEY (`nyelv_id`) REFERENCES `nyelv` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `konyv_ibfk_3` FOREIGN KEY (`mufaj_id`) REFERENCES `mufaj` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `konyvek`
+  ADD CONSTRAINT `konyvek_ibfk_1` FOREIGN KEY (`szerzo_id`) REFERENCES `szerzok` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `konyvek_ibfk_2` FOREIGN KEY (`nyelv_id`) REFERENCES `nyelvek` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `konyvek_ibfk_3` FOREIGN KEY (`mufaj_id`) REFERENCES `mufajok` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Megkötések a táblához `peldany`
+-- Megkötések a táblához `peldanyok`
 --
-ALTER TABLE `peldany`
-  ADD CONSTRAINT `peldany_ibfk_1` FOREIGN KEY (`konyv_id`) REFERENCES `konyv` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `peldanyok`
+  ADD CONSTRAINT `peldanyok_ibfk_1` FOREIGN KEY (`konyv_id`) REFERENCES `konyvek` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Megkötések a táblához `user`
+-- Megkötések a táblához `userek`
 --
-ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`olvaso_id`) REFERENCES `olvaso` (`kartyaszam`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `userek`
+  ADD CONSTRAINT `userek_ibfk_1` FOREIGN KEY (`olvaso_id`) REFERENCES `olvasok` (`kartyaszam`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
