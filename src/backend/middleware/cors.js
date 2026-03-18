b74d7cab9643c859ff4d7cd37b7f01a0b85c6d02
Szoke Mark <Lupika666@gmail.com>
2026-03-18 09:12:31 +0100

    corsPolicy credentials: true added

--- src/backend/middleware/cors.js
+++ src/backend/middleware/cors.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const corsOptions = {
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Accept', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+    credentials: true
 };
 
 module.exports = cors(corsOptions);
